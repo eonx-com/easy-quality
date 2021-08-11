a8fd4435f33f68a05e61fcae8b2b407ad0950ce2
Evgeny Ruban <ev.ruban@gmail.com>
2021-08-11 17:49:49 +0400

    feature/update-ecs-and-rector
    - Fix Sniff.

--- tests/Sniffs/Functions/DisallowNonNullDefaultValueSniff/DisallowNonNullDefaultValueSniffTest.php
+++ tests/Sniffs/Functions/DisallowNonNullDefaultValueSniff/DisallowNonNullDefaultValueSniffTest.php
@@ -19,120 +19,72 @@ final class DisallowNonNullDefaultValueSniffTest extends AbstractCheckerTestCase
         return __DIR__ . '/config/configured_rule.php';
     }
 
-    /**
-     * @throws \ECSPrefix20210805\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     * @throws \RectorPrefix20210706\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     */
     public function testProcessMultiLineParametersInClassMethodSucceeds(): void
     {
         $fileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Correct/ClassMethodMultiLineParameters.php.inc');
         $this->doTestCorrectFileInfo($fileInfo);
     }
 
-    /**
-     * @throws \ECSPrefix20210805\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     * @throws \RectorPrefix20210706\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     */
     public function testProcessMultiLineParametersInClosureSucceeds(): void
     {
         $fileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Correct/ClosureMultiLineParameters.php.inc');
         $this->doTestCorrectFileInfo($fileInfo);
     }
 
-    /**
-     * @throws \ECSPrefix20210805\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     * @throws \RectorPrefix20210706\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     */
     public function testProcessMultiLineParametersInSimpleFunctionSucceeds(): void
     {
         $fileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Correct/SimpleFunctionMultiLineParameters.php.inc');
         $this->doTestCorrectFileInfo($fileInfo);
     }
 
-    /**
-     * @throws \ECSPrefix20210805\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     * @throws \RectorPrefix20210706\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     */
     public function testProcessSingleLineParametersInClassMethodSucceeds(): void
     {
         $fileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Correct/ClassMethodSingleLineParameters.php.inc');
         $this->doTestCorrectFileInfo($fileInfo);
     }
 
-    /**
-     * @throws \ECSPrefix20210805\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     * @throws \RectorPrefix20210706\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     */
     public function testProcessSingleLineParametersInClosureSucceeds(): void
     {
         $fileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Correct/ClosureSingleLineParameters.php.inc');
         $this->doTestCorrectFileInfo($fileInfo);
     }
 
-    /**
-     * @throws \ECSPrefix20210805\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     * @throws \RectorPrefix20210706\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     */
     public function testProcessSingleLineParametersInSimpleFunctionSucceeds(): void
     {
         $fileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Correct/SimpleFunctionSingleLineParameters.php.inc');
         $this->doTestCorrectFileInfo($fileInfo);
     }
 
-    /**
-     * @throws \ECSPrefix20210805\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     * @throws \RectorPrefix20210706\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     */
     public function testProcessWrongMultiLineParametersInClassMethodFails(): void
     {
         $wrongFileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Wrong/ClassMethodMultiLineParameters.php.inc');
         $this->doTestFileInfoWithErrorCountOf($wrongFileInfo, 10);
     }
 
-    /**
-     * @throws \ECSPrefix20210805\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     * @throws \RectorPrefix20210706\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     */
     public function testProcessWrongMultiLineParametersInClosureFails(): void
     {
         $wrongFileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Wrong/ClosureMultiLineParameters.php.inc');
         $this->doTestFileInfoWithErrorCountOf($wrongFileInfo, 10);
     }
 
-    /**
-     * @throws \ECSPrefix20210805\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     * @throws \RectorPrefix20210706\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     */
     public function testProcessWrongMultiLineParametersInSimpleFunctionFails(): void
     {
         $wrongFileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Wrong/SimpleFunctionMultiLineParameters.php.inc');
         $this->doTestFileInfoWithErrorCountOf($wrongFileInfo, 10);
     }
 
-    /**
-     * @throws \ECSPrefix20210805\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     * @throws \RectorPrefix20210706\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     */
     public function testProcessWrongSingleLineParametersInClassMethodFails(): void
     {
         $wrongFileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Wrong/ClassMethodSingleLineParameters.php.inc');
         $this->doTestFileInfoWithErrorCountOf($wrongFileInfo, 3);
     }
 
-    /**
-     * @throws \ECSPrefix20210805\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     * @throws \RectorPrefix20210706\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     */
     public function testProcessWrongSingleLineParametersInClosureFails(): void
     {
         $wrongFileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Wrong/ClosureSingleLineParameters.php.inc');
         $this->doTestFileInfoWithErrorCountOf($wrongFileInfo, 3);
     }
 
-    /**
-     * @throws \ECSPrefix20210805\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     * @throws \RectorPrefix20210706\Symplify\SmartFileSystem\Exception\FileNotFoundException
-     */
     public function testProcessWrongSingleLineParametersInSimpleFunctionFails(): void
     {
         $wrongFileInfo = new SmartFileInfo(__DIR__ . '/Fixtures/Wrong/SimpleFunctionSingleLineParameters.php.inc');
@@ -140,8 +92,9 @@ final class DisallowNonNullDefaultValueSniffTest extends AbstractCheckerTestCase
     }
 
     // @todo Research issue `Error: Undefined constant 'T_CLOSE_SHORT_ARRAY'`
+    /*
     protected function setUp(): void
     {
         $this->markTestSkipped();
-    }
+    }*/
 }
