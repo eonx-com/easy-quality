eb0790fcb563d54caf8b4078a9ba4a1b5012d564
mvlad <vladislav.makar@eonx.com>
2021-11-22 17:41:04 +0300

    [ECS][PHPStan] Add ThrowExceptionMessageRule (#17)
    * [ECS][PHPStan] Add ThrowExceptionMessageRule

---
+++ src/PHPStan/ThrowExceptionMessageRule.php
@@ -0,0 +1,92 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyQuality\PHPStan;
+
+use Nette\Utils\Strings;
+use PhpParser\Node;
+use PhpParser\Node\Expr\New_;
+use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\Throw_;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\ShouldNotHappenException;
+
+final class ThrowExceptionMessageRule implements Rule
+{
+    private const DEFAULT_VALID_PREFIXES = ['exceptions.'];
+
+    public const ERROR_MESSAGE = 'Exception message must be either a variable or a translation message, started with one of [%s]';
+
+    /**
+     * @var string|null
+     */
+    private $exceptionInterface;
+
+    /**
+     * @var string[]|null
+     */
+    private $validPrefixes;
+
+    public function __construct(
+        ?string $exceptionInterface = null,
+        ?array $validPrefixes = null
+    ) {
+        $this->exceptionInterface = $exceptionInterface;
+        $this->validPrefixes = $validPrefixes ?? self::DEFAULT_VALID_PREFIXES;
+    }
+
+
+    public function getNodeType(): string
+    {
+        return Throw_::class;
+    }
+
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if (!$node instanceof Throw_) {
+            throw new ShouldNotHappenException();
+        }
+        $expr = $node->expr;
+
+        if (!$expr instanceof New_ && !$expr instanceof StaticCall) {
+            return [];
+        }
+
+        if ($this->exceptionInterface !== null
+            && !\is_a($expr->class->toString(), $this->exceptionInterface, true)
+        ) {
+            return [];
+        }
+
+        if (count($expr->args) === 0) {
+            return [];
+        }
+
+        $stringNode = $expr->args[0]->value;
+        if (!$stringNode instanceof String_) {
+            return [];
+        }
+        $errors = [];
+        if (!$this->startsWithValidPrefix($stringNode->value)) {
+            $errors[] = \sprintf(
+                self::ERROR_MESSAGE,
+                \implode(', ', $this->validPrefixes)
+            );
+        }
+
+        return $errors;
+    }
+
+    private function startsWithValidPrefix(string $message): bool
+    {
+        foreach ($this->validPrefixes as $validPrefix) {
+            if (Strings::startsWith($message, $validPrefix) === true) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ tests/PHPStan/ThrowExceptionMessageRule/ThrowExceptionMessageRuleTest.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace EonX\EasyQuality\Tests\PHPStan\ThrowExceptionMessageRule;
+
+use EonX\EasyQuality\PHPStan\ThrowExceptionMessageRule;
+use Iterator;
+use PHPStan\Rules\Rule;
+use Symplify\PHPStanExtensions\Testing\AbstractServiceAwareRuleTestCase;
+
+/**
+ * @covers \EonX\EasyQuality\PHPStan\ThrowExceptionMessageRule
+ *
+ * @internal
+ */
+final class ThrowExceptionMessageRuleTest extends AbstractServiceAwareRuleTestCase
+{
+    public function getRule(): Rule
+    {
+        return $this->getRuleFromConfig(
+            ThrowExceptionMessageRule::class,
+            __DIR__ . '/config/configured_rule.neon'
+        );
+    }
+
+    public function provideData(): Iterator
+    {
+        yield [__DIR__ . '/Fixture/correct/anotherExceptionType.php.inc', []];
+
+        yield [__DIR__ . '/Fixture/correct/multilineException.php.inc', []];
+
+        yield [__DIR__ . '/Fixture/correct/noExceptionMessage.php.inc', []];
+
+        yield [__DIR__ . '/Fixture/correct/throwVariable.php.inc', []];
+
+        yield [__DIR__ . '/Fixture/correct/validPrefix.php.inc', []];
+
+        yield [__DIR__ . '/Fixture/correct/variableMessage.php.inc', []];
+
+        $errorMessage = \sprintf(ThrowExceptionMessageRule::ERROR_MESSAGE, 'exceptions.');
+        yield [__DIR__ . '/Fixture/wrong/hardcodedMessage.php.inc', [[$errorMessage, 4]]];
+    }
+
+    /**
+     * @dataProvider provideData()
+     *
+     * @param array<string|int> $expectedErrorMessagesWithLines
+     */
+    public function testRule(string $filePath, array $expectedErrorMessagesWithLines): void
+    {
+        $this->analyse([$filePath], $expectedErrorMessagesWithLines);
+    }
+}
