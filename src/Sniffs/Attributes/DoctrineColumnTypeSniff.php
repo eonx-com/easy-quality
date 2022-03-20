1bf4a4990d1e1418b400ebe84892a29b21cf2a7f
Alexandr Tsepelev <alexandr.tsepelev@eonx.com>
2022-03-20 16:52:59 +0600

    feature/PLU-4059-ecs-datetime-properties-declaration-check
    * DoctrineColumnTypeSniff
    * PropertyTypeSniff

---
+++ src/Sniffs/Attributes/DoctrineColumnTypeSniff.php
@@ -0,0 +1,144 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyQuality\Sniffs\Attributes;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+use SlevomatCodingStandard\Helpers\TokenHelper;
+
+final class DoctrineColumnTypeSniff implements Sniff
+{
+    /**
+     * @var string
+     */
+    private const ERROR_INVALID_COLUMN_TYPE = 'InvalidColumnType';
+
+    public array $replacePairs = [];
+
+    /**
+     * @param int $stackPointer
+     */
+    public function process(File $phpcsFile, $stackPointer): void
+    {
+        if (count($this->replacePairs) === 0) {
+            return;
+        }
+
+        $tokens = $phpcsFile->getTokens();
+
+        $stackPointerEnd = TokenHelper::findNext($phpcsFile, [\T_ATTRIBUTE_END], $stackPointer);
+
+        $columnFound = false;
+        for ($i = $stackPointer; $i <= $stackPointerEnd; $i++) {
+            $currentToken = $tokens[$i];
+
+            if ($currentToken['code'] === \T_STRING && $currentToken['content'] === 'Column') {
+                $columnFound = true;
+            }
+
+            if ($columnFound && $currentToken['code'] === \T_PARAM_NAME && $currentToken['content'] === 'type') {
+                $tokensToReplace = $this->findNextTypesOnly(
+                    $phpcsFile,
+                    [\T_STRING, \T_DOUBLE_COLON, \T_CONSTANT_ENCAPSED_STRING],
+                    ++$i
+                );
+
+                if (count($tokensToReplace) === 0) {
+                    return;
+                }
+
+                $content = \trim(
+                    TokenHelper::getContent(
+                        $phpcsFile,
+                        $tokensToReplace[0],
+                        $tokensToReplace[(int)count($tokensToReplace) - 1]
+                    ),
+                    '"\''
+                );
+
+                if (!isset($this->replacePairs[$content])) {
+                    return;
+                }
+
+                $fix = $phpcsFile->addFixableError(
+                    \sprintf(
+                        'Please avoid using "%s" doctrine column type use "%s" instead',
+                        $content,
+                        $this->replacePairs[$content]
+                    ),
+                    $stackPointer,
+                    self::ERROR_INVALID_COLUMN_TYPE
+                );
+
+                if ($fix !== false) {
+
+                    $this->addChangeset($phpcsFile, $tokensToReplace, $this->replacePairs[$content]);
+
+                }
+            }
+        }
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function register(): array
+    {
+        return [\T_ATTRIBUTE];
+    }
+
+    private function addChangeset(File $phpcsFile, array $tokensToReplace, string $replaceWith): void
+    {
+        $phpcsFile->fixer->beginChangeset();
+
+        $needQuotes = \strpos($replaceWith, '::') === false;
+
+        if ($needQuotes === true) {
+
+            $quote = $this->getQuote($phpcsFile, $tokensToReplace, $replaceWith);
+
+            $replaceWith = $quote . \trim($replaceWith, '"\'') . $quote;
+        }
+
+        foreach ($tokensToReplace as $index => $tokenPos) {
+            $phpcsFile->fixer->replaceToken($tokenPos, $index === 0 ? $replaceWith : '');
+        }
+
+        $phpcsFile->fixer->endChangeset();
+    }
+
+    private function findNextTypesOnly(File $phpcsFile, array $types, int $startPos): array
+    {
+        $foundPositions = [];
+        $tokens = $phpcsFile->getTokens();
+        for ($pos = $startPos; $pos <= \array_key_last($tokens); $pos++) {
+            $token = $tokens[$pos];
+            $isRequiredToken = \in_array($token['code'], $types, true) === true;
+            if ($isRequiredToken === true) {
+                $foundPositions[] = $pos;
+            } elseif (count($foundPositions) > 0) {
+                break;
+            }
+        }
+
+        return $foundPositions;
+    }
+
+    private function getQuote(File $phpcsFile, array $tokensToReplace, string $replaceWith): string
+    {
+        $foundQuote = \mb_substr($replaceWith, 0, 1);
+        if ($foundQuote === '' || \in_array($foundQuote, ['"', "'"], true) === false) {
+            $firstReplacebleToken = $phpcsFile->getTokens()[$tokensToReplace[0]];
+            if ($firstReplacebleToken['code'] === \T_CONSTANT_ENCAPSED_STRING) {
+                $foundQuote = \mb_substr($firstReplacebleToken['content'], 0, 1);
+                if (\in_array($foundQuote, ['"', "'"], true) === false) {
+                    $foundQuote = '"';
+                }
+            }
+        }
+
+        return $foundQuote;
+    }
+}
---
+++ src/Sniffs/Classes/PropertyTypeSniff.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyQuality\Sniffs\Classes;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\AbstractVariableSniff;
+
+final class PropertyTypeSniff extends AbstractVariableSniff
+{
+    /**
+     * @var string
+     */
+    private const ERROR_INVALID_TYPE = 'InvalidType';
+
+    public array $replacePairs = [];
+
+    /**
+     * @param int $stackPtr
+     */
+    protected function processMemberVar(File $phpcsFile, $stackPtr): void
+    {
+        try {
+            $propertyInfo = $phpcsFile->getMemberProperties($stackPtr);
+
+            if (count($propertyInfo) === 0) {
+                return;
+            }
+        } catch (\Throwable) {
+            return;
+        }
+
+        $normalizedType = $this->normalizePropertyType($propertyInfo['type']);
+        if (!isset($this->replacePairs[$normalizedType])) {
+            return;
+        }
+
+        $fix = $phpcsFile->addFixableError(
+            \sprintf(
+                'Please avoid using "%s" property type, use "%s" instead',
+                $normalizedType,
+                $this->replacePairs[$normalizedType]
+            ),
+            $propertyInfo['type_token'],
+            self::ERROR_INVALID_TYPE
+        );
+
+        if ($fix !== false) {
+
+            $phpcsFile->fixer->beginChangeset();
+
+            for ($i = $propertyInfo['type_token']; $i <= $propertyInfo['type_end_token']; $i++) {
+                $phpcsFile->fixer->replaceToken(
+                    $i,
+                    $i === $propertyInfo['type_token'] ? $this->replacePairs[$normalizedType] : ''
+                );
+            }
+
+            $phpcsFile->fixer->endChangeset();
+        }
+    }
+
+    /**
+     * @param int $stackPtr
+     */
+    protected function processVariable(File $phpcsFile, $stackPtr): void
+    {
+        // No needed for sniff.
+    }
+
+    /**
+     * @param int $stackPtr
+     */
+    protected function processVariableInString(File $phpcsFile, $stackPtr): void
+    {
+        // No needed for sniff.
+    }
+
+    private function normalizePropertyType(string $propertyType): string
+    {
+        $parts = \explode('\\', $propertyType);
+
+        return \ltrim($parts[(int)\count($parts) - 1], '?');
+    }
+}
