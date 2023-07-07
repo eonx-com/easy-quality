7a81d4135a535c3228fa0695da612d5672ae3db1
Evgeny Ruban <ev.ruban@gmail.com>
2023-07-07 19:59:06 +0400

    feature/use-phpmetrics
    -

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 
+/* Trying to fix the rector tests issue. It works but on second run.
 $contents = \file_get_contents(__DIR__ . '/../vendor/composer/autoload_static.php');
 $contents = \str_replace(
     "'5f0e95b8df5acf4a92c896dc3ac4bb6e' => __DIR__ . '/..' . '/phpmetrics/phpmetrics/src/functions.php',",
@@ -8,6 +9,7 @@ $contents = \str_replace(
     $contents
 );
 \file_put_contents(__DIR__ . '/../vendor/composer/autoload_static.php', $contents);
+*/
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/symplify/easy-coding-standard/vendor/squizlabs/php_codesniffer/autoload.php';
