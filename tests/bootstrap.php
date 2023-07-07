bf8d440ce8e28297d87da48a3c4c88758c56e693
Evgeny Ruban <ev.ruban@gmail.com>
2023-07-07 16:34:53 +0400

    feature/use-phpmetrics
    -

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,16 @@
 <?php
 declare(strict_types=1);
 
-// Require_once __DIR__ . '/../vendor/rector/rector/preload.php';
-// require_once __DIR__ . '/../vendor/rector/rector/vendor/autoload.php';
+/* workaround
+$contents = \file_get_contents(__DIR__ . '/../vendor/composer/autoload_static.php');
+$contents = \str_replace(
+    "'5f0e95b8df5acf4a92c896dc3ac4bb6e' => __DIR__ . '/..' . '/phpmetrics/phpmetrics/src/functions.php',",
+    '',
+    $contents
+);
+\file_put_contents(__DIR__ . '/../vendor/composer/autoload_static.php', $contents);
+*/
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/symplify/easy-coding-standard/vendor/squizlabs/php_codesniffer/autoload.php';
 // @todo remove after resolving https://github.com/symplify/symplify/issues/4461
