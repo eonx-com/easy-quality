25528b6d5361673dab84938d16f39f76a40c942a
Evgeny Ruban <ev.ruban@gmail.com>
2023-07-13 19:31:29 +0400

    feature/use-phpmetrics
    -

---
+++ tests/autoload-patch.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# This script removes `/phpmetrics/phpmetrics/src/functions.php` file from autoload
+# because it causes a fatal error when running rector tests
+
+awk '!/5f0e95b8df5acf4a92c896dc3ac4bb6e/' ../vendor/composer/autoload_static.php > temp && mv temp ../vendor/composer/autoload_static.php
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,6 @@
 <?php
 declare(strict_types=1);
 
-/* Trying to fix the rector tests issue. It works but on second run.
-$contents = \file_get_contents(__DIR__ . '/../vendor/composer/autoload_static.php');
-$contents = \str_replace(
-    "'5f0e95b8df5acf4a92c896dc3ac4bb6e' => __DIR__ . '/..' . '/phpmetrics/phpmetrics/src/functions.php',",
-    '',
-    $contents
-);
-\file_put_contents(__DIR__ . '/../vendor/composer/autoload_static.php', $contents);
-*/
-
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/symplify/easy-coding-standard/vendor/squizlabs/php_codesniffer/autoload.php';
 // @todo remove after resolving https://github.com/symplify/symplify/issues/4461
