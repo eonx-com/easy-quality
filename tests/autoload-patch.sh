00821d4995ad66f82e1e96315e2470099ad93aa0
Aleksey Polyvanyi <alexndlm@gmail.com>
2024-12-15 15:15:37 +0100

    -refactoring

--- tests/autoload-patch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# This script removes `/phpmetrics/phpmetrics/src/functions.php` file from autoload
-# because it causes a fatal error when running rector tests.
-#
-# Removes this line `'5f0e95b8df5acf4a92c896dc3ac4bb6e' => __DIR__ . '/..' . '/phpmetrics/phpmetrics/src/functions.php',`
-# from `./vendor/composer/autoload_static.php` file.
-
-awk '!/5f0e95b8df5acf4a92c896dc3ac4bb6e/' ./vendor/composer/autoload_static.php > temp && mv temp ./vendor/composer/autoload_static.php
